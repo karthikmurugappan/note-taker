84b905d96e375cdc58a6355d5dec272117dd6cb5
karthikmurugappan <karthikmurugappan7@gmail.com>
2023-07-24 00:41:02 -0400

    updated get route

--- server.js
+++ server.js
@@ -17,3 +17,18 @@ app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
 // Define routes
 const sendIndexHtml = (res) => res.sendFile(path.join(__dirname, "/public/index.html"));
 
+app.get("/index", sendIndexHtml);
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "/public/notes.html")));
+app.get("/", sendIndexHtml);
+
+// API ROUTES
+
+//GET Route
+app.get("/api/notes", (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (error, file) => {
+        if (error) throw error;
+        const parsedFile = JSON.parse(file);
+        return res.send(parsedFile);
+    });
+});
+
